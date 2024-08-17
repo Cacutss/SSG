de7aba4bd34db0cd3a4c45d2a10207a435219164
Cacutss <cuentasmurfdeleague@gmail.com>
2024-08-17 12:00:42 -0300

    textnode to html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,6 @@
+from textnode import TextNode
 class HTMLNODE:
-    def __init__(self,tag = None,value = None,children:list = None,**kwargs):
+    def __init__(self,tag = None,value = None,children = None,**kwargs):
         self.tag = tag
         self.value = value
         self.children = children
@@ -24,26 +25,18 @@ class LeafNode(HTMLNODE):
     def __init__(self,tag = None,value = None,**kwargs):
         super().__init__(tag,value,**kwargs)
 
-     
-    def props_to_html(self):
-        if self.props == {}:
-            return ""
-        props = ""
-        for key,value in self.props.items():
-            props += f" {key}={value}"
-        return props
-    
     def to_html(self):
         if self.value == None:
             raise ValueError("No value")
         if self.tag == None:
             return self.value
         else:
-            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+            return f"<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>"
+        
         
 class ParentNode(HTMLNODE):
-    def __init__(self,tag = None,children:list = None,**kwargs):
-        super().__init__(tag,children,**kwargs)
+    def __init__(self,tag = None,value= None,children:list[LeafNode] = None,**kwargs):
+        super().__init__(tag,value,children,**kwargs)
 
     def to_html(self):
         if self.tag == None:
@@ -51,7 +44,29 @@ class ParentNode(HTMLNODE):
         if self.children == None:
             raise ValueError("NO CHILDREN, NO PARENT")
         string = ""
-        string += self.children[0].to_html()
-        
+        for child in self.children:
+            string += child.to_html()
+        return f"<{self.tag}{super().props_to_html()}>{string}</{self.tag}>"
+
+
+    
     def __repr__(self):
-        return f"tag = {self.tag}, children = {self.children}, props = {self.props}"
+        return f"tag = {self.tag},value = {self.value}, children = {self.children}, props = {self.props}"
+    
+def text_node_to_html_node(text_node:TextNode):
+    match(text_node.text_type):
+        case "bold":
+            return LeafNode(tag = "b",value = text_node.text)
+        case "text":
+            return LeafNode(value = text_node.text)
+        case "italic":
+            return LeafNode(tag = "i",value = text_node.text)
+        case "code":
+            return LeafNode(tag= "code",value = text_node.text)
+        case "link":
+            return LeafNode(tag= "a",value = text_node.text,href = text_node.url)
+        case "image":
+            return LeafNode(tag = "img",value = "",src = text_node.url,alt = text_node.text)
+        case _:
+            raise Exception("Unsupported type")
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,10 +1,14 @@
 import unittest
 
+from textnode import TextNode
+
 from htmlnode import HTMLNODE
 
 from htmlnode import LeafNode
 
 from htmlnode import ParentNode
+
+from htmlnode import text_node_to_html_node
 
 class TestHTMLNODE(unittest.TestCase):
     def test_props(self):
@@ -21,9 +25,20 @@ class TestHTMLNODE(unittest.TestCase):
         print(node3.to_html())
 
     def test_tohtmlparent(self):
-        node2 = LeafNode("2","3")
-        node1 = ParentNode(tag = "h2",a= "fart",b ="schizo")
-        print(node1)
+        node2 = LeafNode(tag = "2",value= "3")
+        node3 = ParentNode(tag= "p",children= [LeafNode(tag = "h1",value= "None")])
+        node1 = ParentNode(tag = "h2",children = [node2,node3],a= "fart",b ="schizo")
+        node4 = ParentNode(tag = "furry",children=[LeafNode(tag= "hello kitty",value="fire")], value= "nothing", a= "mondongo")
+        print(node1.to_html())
+        print(node4.to_html())
+
+    def test_textnode_tohtml(self):
+        textnode1 = TextNode(text = "joder", text_type= "bold")
+        textnode2 = TextNode(text = "", text_type= "italic", url="joder.com")
+        textnode3 = TextNode(url="fuera" ,text_type="image")
+        print(text_node_to_html_node(textnode1).to_html())
+        print(text_node_to_html_node(textnode2).to_html())
+        print(text_node_to_html_node(textnode3).to_html())
 
 if __name__ == "__main__":
     unittest.main()
